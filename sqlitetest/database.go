1947e188494067e209173d175cde473b8130917a
Markus Wüstenberg <markus@maragu.dk>
2025-11-03 14:47:17 +0100

    Add database fixtures helper

--- sqlitetest/database.go
+++ sqlitetest/database.go
@@ -9,9 +9,17 @@ import (
 	"app/sqlite"
 )
 
-// NewDatabase for testing.
-func NewDatabase(t *testing.T) *sqlite.Database {
+// NewDatabase for testing, with optional options.
+// Options:
+// - [WithFixtures] to load fixtures after migration.
+func NewDatabase(t *testing.T, opts ...NewDatabaseOption) *sqlite.Database {
 	t.Helper()
 
-	return sqlite.NewDatabase(sqlite.NewDatabaseOptions{H: sqlitetest.NewHelper(t)})
+	return sqlite.NewDatabase(sqlite.NewDatabaseOptions{H: sqlitetest.NewHelper(t, opts...)})
+}
+
+type NewDatabaseOption = sqlitetest.NewHelperOption
+
+func WithFixtures(fixtures ...string) sqlitetest.NewHelperOption {
+	return sqlitetest.WithFixtures(fixtures...)
 }
